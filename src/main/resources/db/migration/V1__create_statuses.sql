376c2f0736fdcf6c1e6bd5ade50ded428a3e2e62
Dmitry Bobrov <bobrowww@mail.ru>
2026-03-31 19:44:10 +0500

    feat: add status position with database migration

---
+++ src/main/resources/db/migration/V1__create_statuses.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS statuses (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(25) NOT NULL
+    );
+
+INSERT INTO statuses (title)
+VALUES
+    ('доступен'),
+    ('арендован'),
+    ('на техобслуживании'),
+    ('неисправен');
