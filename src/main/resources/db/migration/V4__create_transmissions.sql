9fe9f66a2e23dfdea17079cca0b8d739afc257d2
Dmitry Bobrov <bobrowww@mail.ru>
2026-04-01 19:48:52 +0500

    feat: add vehicle drive entity with database migration

--- src/main/resources/db/migration/V4__create_transmissions.sql
+++ src/main/resources/db/migration/V4__create_transmissions.sql
@@ -1,10 +1,11 @@
 CREATE TABLE IF NOT EXISTS transmissions (
     id SERIAL PRIMARY KEY,
-    title VARCHAR(10) NOT NULL
+    title VARCHAR(20) NOT NULL
 );
 
 INSERT INTO transmissions (title)
 VALUES
-    ('передний'),
-    ('задний'),
-    ('полный');
+    ('механическая'),
+    ('автомат'),
+    ('роботизированная'),
+    ('вариатор');
